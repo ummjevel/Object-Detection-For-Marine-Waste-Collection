4e212433f91a4ba3921da020a484fee0303868d1
sjjeong <sjjeong@cubox.ai>
2023-04-21 20:29:07 +0900

    modify train

--- train.py
+++ train.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python3
+
 import torch
 from ultralytics import YOLO
 
 
 def main():
-    model = YOLO('yolov8n.pt', device='gpu')
+    model = YOLO('runs/detect/yolov8n_underwater/weights/best.pt') # YOLO('yolov8n.pt')
     model.to('cuda')
     
     results = model.train(
         data='underwater_cuda.yaml',
         imgsz=640,
         epochs=100,
-        patience=5,
-        batch=16,
+        patience=30,
+        batch=32,
         name='yolov8n_underwater',
         device=0,
         optimizer='Adam'
